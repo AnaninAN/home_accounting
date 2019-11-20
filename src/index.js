172c52cf3f88c71531eae1fc9d2b13ca6e24dab7
AnaninAN <ananinpgu@mail.ru>
2019-11-21 00:04:01 +0300

    start webpack + react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/main.scss';
+
+const App = () => <p>Hello! This is the start of the cool "Home accounting" app</p>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
