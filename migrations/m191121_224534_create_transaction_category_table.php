8e240dd3a8463d6043bc81734f119a6bd7c00c1e
Boris Aniskevich <aniskevich.bo@gmail.com>
2019-11-22 18:47:24 +1000

    migrations PK fix

--- migrations/m191121_224534_create_transaction_category_table.php
+++ migrations/m191121_224534_create_transaction_category_table.php
@@ -13,9 +13,30 @@ class m191121_224534_create_transaction_category_table extends Migration
     public function safeUp()
     {
         $this->createTable('{{%transaction_category}}', [
-            'transaction_id' => $this->primaryKey(),
-            'category_id' => $this->integer()
+            'transaction_id' => $this->integer()->notNull(),
+            'category_id' => $this->integer()->notNull()
         ]);
+
+        $this->addPrimaryKey(
+            'pk_transaction_category',
+            '{{%transaction_category}}',
+            ['transaction_id', 'category_id']);
+
+        $this->addForeignKey(
+            'fk_transaction_category_transaction_id',
+            '{{%transaction_category}}',
+            'transaction_id',
+            'transaction',
+            'id',
+            'CASCADE');
+
+        $this->addForeignKey(
+            'fk_transaction_category_category_id',
+            '{{%transaction_category}}',
+            'category_id',
+            'category',
+            'id',
+            'CASCADE');
     }
 
     /**
@@ -23,6 +44,9 @@ class m191121_224534_create_transaction_category_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropForeignKey('fk_transaction_category_transaction_id', '{{%transaction_category}}');
+        $this->dropForeignKey('fk_transaction_category_category_id', '{{%transaction_category}}');
+
         $this->dropTable('{{%transaction_category}}');
     }
 }
