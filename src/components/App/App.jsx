a31d8e6b5be1a760c12c755b409cca73c6030ebc
AnaninAN <ananinpgu@mail.ru>
2020-02-12 23:05:28 +0300

    App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,19 +4,23 @@ import { Header } from 'containers/Header';
 import { PrivateRoute } from 'components/PrivateRoute';
 import { DashboardRedux } from 'containers/Dashboard';
 
-class App extends PureComponent {
-  state = {token: localStorage.getItem('token')};
+export class App extends PureComponent {
+  state = {
+    token: localStorage.getItem('token'),
+  };
 
   handleSignOut = () => {
-  this.setState({token: null}, () => {
     localStorage.clear();
-  });
+    this.setState({
+      token: '',
+    });
   };
 
   render() {
     return (
       <div>
         <Header token={this.state.token} handleSignOut={this.handleSignOut}/>
+        <PrivateRoute path='/' component={DashboardRedux} exact/>
       </div>
     );
   }
