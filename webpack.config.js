c35cf7d1e788c43aba96be66c43942ab2bef067a
AnaninAN <ananinpgu@mail.ru>
2020-02-13 00:38:35 +0300

    Добавил настройку в конфиг для отслеживания ошибок в исходном файле

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = (env = {}) => {
     mode: isProd ? 'production' : isDev && 'development',
 
     entry: path.resolve(__dirname, 'src', 'index.js'),
+    devtool: isDev ? 'eval-source-map' : 'none',
     output: {
       filename: isProd ? 'main-[hash:8].js' : undefined
     },
