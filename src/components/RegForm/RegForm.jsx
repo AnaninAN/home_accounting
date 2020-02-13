fa5208913c03a8ee268762a01f67e4903dae93c7
AnaninAN <ananinpgu@mail.ru>
2020-02-13 22:53:55 +0300

    Форма для регистрации пользователя

---
+++ src/components/RegForm/RegForm.jsx
@@ -0,0 +1,57 @@
+import './RegForm.scss';
+
+import React, { PureComponent } from 'react';
+import { Container, Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
+
+export class RegForm extends PureComponent {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+  };
+
+  handleTextEdit = ({ target: {name, value} }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSignUp = (event) => {
+    const { handleSignUp  } = this.props;
+
+    event.preventDefault();
+    handleSignUp(this.state);
+  };
+
+  render() {
+    const { username, password, email } = this.state;
+    const { errors } = this.props;
+
+    return (
+      <Container>
+        <Form className='mt-5 border p-5 d-flex flex-column justify-content-center'>
+          <h3 className='border-bottom text-center pb-2 mb-3'>Please Sign Up</h3>
+          <FormGroup>
+            <Label for='username'>Username</Label>
+            <Input type='text' name='username' onChange={this.handleTextEdit} value={username}/>
+          </FormGroup>
+          <FormGroup>
+            <Label for='password'>Password</Label>
+            <Input type='password' name='password' onChange={this.handleTextEdit} value={password}/>
+          </FormGroup>
+          <FormGroup>
+            <Label for='email'>Email</Label>
+            <Input type='text' name='email' onChange={this.handleTextEdit} value={email}/>
+          </FormGroup>
+          {
+              errors.size > 0 &&
+              <Alert color="danger">
+                {errors.get('login')}
+              </Alert>
+          }
+          <Button color='success' onClick={this.handleSignUp}>Sign Up</Button>
+        </Form>
+      </Container>
+    );
+  }
+}
